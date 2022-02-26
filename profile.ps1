9b87845d51dc086ec6c4c964f3295da61d04bda1
egemencanguler <egemencanguler@gmail.com>
2022-02-26 23:41:57 +0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -7,6 +7,10 @@ $Documents = "C:\Users\USER\Documents"
 $UnityProjects = "C:\Users\USER\Documents\UnityProjects"
 $Notes = "C:\Users\USER\Documents\Sync\notes"
 
+$UrlSetup = "https://github.com/egemencanguler/setup/"
+$UrlWiki = "https://github.com/dalakgames/dalak_wiki"
+$UrlGit = "https://github.com/dalakgames/dalak_wiki/blob/main/git_cheat_sheet.md"
+
 
 function text($path)
 {
@@ -22,3 +26,28 @@ function godocuments(){cd $Documents}
 function gogame(){cd $UnityProjects}
 function gonotes(){text($Notes)}
 function open($path){explorer.exe $path}
+
+
+function GetCurrentFolderName()
+{
+    Split-Path -Path (Get-Location) -Leaf
+}
+
+function todalak()
+{
+    $folderName = GetCurrentFolderName
+    cd $folderName"\Assets\Dalak"
+}
+
+function exitdalak()
+{
+    cd ..
+    cd ..
+    cd ..
+}
+
+function gitupdatesubmodul()
+{
+    git submodule update --init --recursive
+}
+
